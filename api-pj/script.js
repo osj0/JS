ddc845bd6f03579ec1f8cfef1a1888059f7524bf
OSJ <tmdwn119@gmail.com>
2025-02-25 00:22:47 +0900

    url 수정

--- api-pj/script.js
+++ api-pj/script.js
@@ -47,7 +47,10 @@ window.addEventListener("resize", checkDevice);
 //api 불러오기
 const getLatestNews = async()=>{
 
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=20`
+      );
     const response = await fetch(url);
     const data = await response.json()
     newsList = data.articles
